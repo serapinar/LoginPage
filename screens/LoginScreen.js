b77206f5414c78b3c3849643e67de244c745c4b8
Serap Kececi <serapkececi@gmail.com>
2023-12-14 00:43:02 +0300

    CSS of Login Page done

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,46 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView } from 'react-native';
-import React from 'react';
-import { TextInput } from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import {StyleSheet, Text, View, KeyboardAvoidingView, Image} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput} from 'react-native';
+import {TouchableOpacity} from 'react-native';
+import {auth} from "../firebase";
 
 export default function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSignUp = () => {
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then(userCredentials => {
+            const user = userCredentials.user;
+            console.log("USER", user, email);
+        })
+            .catch( (error)=> alert (error.message));
+    };
     return (
         <KeyboardAvoidingView style={styles.container} behavior="padding">
+                <Image
+                    style={styles.tinyLogo}
+                    source={{
+                        uri: 'https://reactnative.dev/img/tiny_logo.png',
+                    }}
+                />
             <View style={styles.inputContainer}>
-                <TextInput style={styles.input} placeholder="Email" />
-                <TextInput style={styles.input} placeholder="Şifre" />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}/>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    secureTextEntry value={password}
+                    onChangeText={text => setPassword(text)}/>
             </View>
             <View style={styles.buttonContainer}>
                 <TouchableOpacity style={styles.button}>
                     <Text style={styles.buttonText}>LOGIN</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={[styles.button, styles.outlineButton]}>
+                <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.outlineButton]}>
                     <Text style={styles.outlineButtonText}>SIGN UP</Text>
                 </TouchableOpacity>
             </View>
@@ -24,7 +50,7 @@ export default function LoginScreen() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor:"#e16702",
+        backgroundColor: "#e16702",
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
@@ -61,5 +87,10 @@ const styles = StyleSheet.create({
     outlineButtonText: {
         color: '#0782F9',
         fontSize: 16,
-    }
+    },
+    tinyLogo: {
+        width: 150,
+        height: 150,
+        marginBottom:20
+    },
 });
